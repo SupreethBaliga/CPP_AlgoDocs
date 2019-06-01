2d42cea7c66bfc5440fd313076774898f90c2804
Supreeth Baliga <42595858+SupreethBaliga@users.noreply.github.com>
2019-06-01 11:26:13 +0530

    Algo corrected

--- nCr.cpp
+++ nCr.cpp
@@ -3,7 +3,7 @@ long int nCr(long int n,long int r)			//change the data-type as per requirement
 	long int ans=1;
 	for(long int i=1; i<=r ; i++)
 	{
-		ans*=( ans* ( n-i+1 ) / i);
+		ans=( ans* ( n-i+1 ) / i);
 	}
 	return ans;
 }
