dbed183ef84daddbaa1a61cda5ac02a9c5d9db1d
Supreeth Baliga <supreethbaliga@gmail.com>
2019-10-09 17:56:34 +0530

    Code for Segment tree has been added.

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,6 +1,6 @@
 /*************************BALIGA***************************************/
 
-// FENWICK SAMPLE CODE FOR SUM QUERY//
+//This is a sample code for segment tree. (Sum of range query)
 #include <bits/stdc++.h>
 // use gp_hash_table<int,int,custom_hash> m;
 // in place of map
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace __gnu_pbds;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());//use rng()%n for numbers in range [0,n-1]
-// This custom_hash is for unordered_map 		
+// This custom_hash is for unordered_map 
 // use map<int, int, custom_hash> mp;
  
 struct custom_hash {
@@ -53,55 +53,75 @@ struct custom_hash {
 #define SIZE 1000000000
 #define E5 100000
 #define E6 1000000
-#define E9 1000000000
 #define INF64 1e18
 #define INF32 1e9
 
-//FENWICK TREE FOR THE SUM QUERY
+//SEGMENT TREEE SUM QUERY SAMPLE
+
+void constructSegment(vi &nums,vli &segment,int low,int high,int pos) {
+	if(low==high) {
+		segment[pos]=nums[low];
+		return;
+	}
+	int mid= (high+low)/2;
+	constructSegment(nums,segment,low,mid,2*pos);
+	constructSegment(nums,segment,mid+1,high,2*pos+1);
+	segment[pos]=(segment[2*pos]+segment[2*pos + 1]);
+	return;
+}
+
+lli sumQuery(vli &segment,int qlow,int qhigh,int low,int high,int pos) {
+	if(qlow<=low && qhigh>=high) return segment[pos];
+	if(qlow>high || qhigh<low) return 0;
+	int mid=(low+high)/2;
+	return (sumQuery(segment,qlow,qhigh,low,mid,2*pos)+sumQuery(segment,qlow,qhigh,mid+1,high,2*pos+1));
+}
+
+void updateSegment(vli &segment,int low,int high,int diff,int idx,int pos) {
+	if(idx<low || idx>high) return;
+	segment[pos]+=diff;
+	if(low!=high) {
+		int mid=(low+high)/2;
+		updateSegment(segment,low,mid,diff,idx,2*pos);
+		updateSegment(segment,mid+1,high,diff,idx,2*pos+1);
+	}
+	return;
+}
+
 void solve() {
 	int n,q1,q2;
 	cin>>n>>q1>>q2;
-	vi nums(n);
-	rep0(i,n) cin>>nums[i];
-	vi fenwick(n+1);
-	//Constructing the tree
-	rep1(i,n) fenwick[i]=0;
-	int idx,val,diff;
-	for(int i=0;i<n;i++) {
-		idx=i+1;
-		while(idx<=n) {
-			fenwick[idx]+=nums[i];
-			idx+=(idx&(-idx));
-		}
-	}
-	//end of constructing tree
 
-	//Updating the tree
+	int N=1;
+	while(N<n) N*=2; //Making size of array a power of 2
+	
+	vi nums(N+1,0);
+	rep1(i,n) cin>>nums[i];
+
+	//Creating the segment tree
+	vli segment(2*N);
+	constructSegment(nums,segment,1,N,1);
+	// rep1(i,2*N-1) cout<<segment[i]<<'\n';
+	//End of creating segment tree
+
+	//Updating segment tree
+	lli pos,val,diff; //pos is one indexed
 	while(q1--) {
-		cin>>idx>>val;
-		diff=val-nums[idx];
-		idx++;
-		while(idx<=n) {
-			fenwick[idx]+=diff;
-			idx+=(idx&(-idx));
-		}
+		cin>>pos>>val;
+		diff=val-nums[pos];
+		updateSegment(segment,1,N,diff,pos,1);
 	}
-	//End of Updating tree
+	//End of updating segment tree
 
-	int sum;
-	//Getting sum queries
+	//Getting sum queries in segment tree
+	lli l,r,ans; //one-indexed
 	while(q2--) {
-		sum=0;
-		cin>>idx;
-		idx++;
-		while(idx>0) {
-			sum+=fenwick[idx];
-			idx-=(idx&(-idx));
-		}
-		cout<<sum<<'\n';
+		cin>>l>>r;
+		ans=sumQuery(segment,l,r,1,N,1);
+		cout<<ans<<'\n';
 	}
-	//End of getting sum queries
-} 
+	//End of queries
+}	
 
 int main()
 {
@@ -113,10 +133,10 @@ int main()
 	#endif
  	int TC;
  	//FOR CODECHEF
- 	// cin>>TC;
+ 	cin>>TC;
  	//
  	//FOR CODEFORCES
- 	TC=1;
+ 	// TC=1;
  	for(int t=0;t<TC;t++) solve();
  	cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
     return 0;
